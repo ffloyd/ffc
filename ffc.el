70a38f5d51689de0f0d7e34cbcf2f56ef819cf50
Roman Kolesnev <rvkolesnev@gmail.com>
2018-01-25 00:39:37 +0300

    Fix another typo

--- ffc.el
+++ ffc.el
@@ -4,7 +4,7 @@
 
 ;; Author: Roman Kolesnev <rvkolesnev@gmail.com>
 ;; Maintainer: Roman Kolesnev <rvkolesnev@gmail.com>
-;; URL: https://github.com/ffloyd/ffe-config
+;; URL: https://github.com/ffloyd/ffc
 ;; Version: 0.0.1
 ;; Package-Requires: ((emacs "25.1") (pkg-info "0.4"))
 ;; Created: 8 Jan 2018
